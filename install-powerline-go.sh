98dacf8f0aae40eaa41be8432ce42fe3a82827f3
GermanVR <german_1241@hotmail.com>
2022-05-18 18:02:32 -0500

    Add verification to package golang

--- install-powerline-go.sh
+++ install-powerline-go.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-
+go_installed="$(apt-cache policy golang-go | grep Instal)"
 powerline_installed="$(cat  ~/.bashrc | grep "function _update_ps1() {")"
+
 
 echo "Validating: [$powerline_installed]"
 if [[ -z "$powerline_installed" ]]; then
     echo "Powerline is Not Installed"
+    echo "Validate Go is installed: [$go_installed]"
+    
+    if [[ -z "$go_installed" ]]; then 
+        sudo sh -c  "apt update -y && apt install golang-go -y"
+        go get -u github.com/justjanne/powerline-go
+    fi
 
-
-sudo sh -c  "apt update -y && apt install golang-go -y"
-
-go get -u github.com/justjanne/powerline-go
-
-cat << \EOT  >> ~/.bashrc 
-GOPATH=$HOME/go
-function _update_ps1() {
-    PS1="$($GOPATH/bin/powerline-go -error $?)"
-}
-if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
-    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
-fi
+    cat << \EOT  >> ~/.bashrc 
+    GOPATH=$HOME/go
+    function _update_ps1() {
+        PS1="$($GOPATH/bin/powerline-go -error $?)"
+    }
+    if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+        PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+    fi
 EOT
 
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
